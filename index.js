b48bbe627537d732db7cfe403ca4a706371a7f6a
G. Andrew Duthie <devhammer@live.com>
2016-10-08 10:10:49 -0400

    Wire up Electron in index.js and load index.html

---
+++ index.js
@@ -0,0 +1,13 @@
+const electron = require('electron');
+const {app} = electron;
+const {BrowserWindow} = electron;
+
+let mainWindow;
+
+app.On('ready', () => {
+    mainWindow = new BrowserWindow({
+        width: 1280,
+        height: 720
+    });
+ mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
